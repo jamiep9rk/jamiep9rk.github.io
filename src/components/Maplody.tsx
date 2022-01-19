785016a32fb18afe7b4fad2cc6545b0cee4be2a4
jamiep9rk <jamiecareer96@gmail.com>
2022-01-19 15:30:32 +0900

    ADD: Skills title css 수정, Maplody 사진 slider 추가, Projects stack css 수정

--- src/components/Maplody.tsx
+++ src/components/Maplody.tsx
@@ -3,8 +3,21 @@ import maplody from "../img/maplody-landing.png";
 import homeIcon from "../img/skills/home-icon.png";
 import githubIcon from "../img/skills/github-icon.png";
 import diaryIcon from "../img/skills/diary-icon.png";
+import signup from "../img/projects/maplody-signup.gif";
+import register from "../img/projects/maplody-register.gif";
+import withdrawal from "../img/projects/maplody-withdrawal.gif";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Maplody() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
     return (
         <div className="projects-container">
             <nav className="project-white-container">
@@ -12,17 +25,24 @@ function Maplody() {
                 <div className="project-team">Front-end 2명 / Back-end 2명</div>
                 <div className="projects-term">2021.11 - 2021.11 (2주)</div>
                 <nav className="project-info-container">
-                    <img className="project-landing-image" src={maplody} alt="" />
+                    <div className="page-carousel">
+                        <Slider {...settings}>
+                            <img className="project-landing-image" src={maplody} alt="" />
+                            <img className="project-landing-image" src={signup} alt="" />
+                            <img className="project-landing-image" src={register} alt="" />
+                            <img className="project-landing-image" src={withdrawal} alt="" />
+                        </Slider>
+                    </div>
                     <div className="project-details-container">
                         <nav className="details-title">🎵 추억이 있는 음악을 들었던 장소와 함께 기록하는 앱, Maplody 입니다</nav>
-                        <ul>원하는 장소에 음악과 사연을 저장할 수 있어요</ul>
-                        <ul>사이드바에서 내가 저장한 음악과 사연을 모아볼 수 있어요</ul>
-                        <ul>기록한 핀을 지도에서 한 눈에 볼 수 있어요</ul>
-                        <ul>저장해놓은 음악을 유튜브 영상으로 감상할 수 있어요</ul>
+                        <li className="details-sentence">원하는 장소에 음악과 사연을 저장할 수 있어요</li>
+                        <li className="details-sentence">사이드바에서 내가 저장한 음악과 사연을 모아볼 수 있어요</li>
+                        <li className="details-sentence">기록한 핀을 지도에서 한 눈에 볼 수 있어요</li>
+                        <li className="details-sentence">저장해놓은 음악을 유튜브 영상으로 감상할 수 있어요</li>
                         <nav className="details-title">✏️ 주요 기능 구현</nav>
-                        <ul>프로젝트 기획 및 진행 상황 관리, 팀원들 간 소통 관리</ul>
-                        <ul>Youtube API를 이용해 검색을 통한 유튜브 비디오 불러오기 구현</ul>
-                        <ul>Google Maps API를 이용한 지도 및 인포 윈도우, 마커 좌표 구현</ul>
+                        <li className="details-sentence">프로젝트 기획 및 진행 상황 관리, 팀원들 간 소통 관리</li>
+                        <li className="details-sentence">Youtube API를 이용해 검색을 통한 유튜브 비디오 불러오기 구현</li>
+                        <li className="details-sentence">Google Maps API를 이용한 지도 및 인포 윈도우, 마커 좌표 구현</li>
                         <nav className="project-stack-container">
                             <div className="stack-box">#React</div>
                             <div className="stack-box">#Javascript</div>
--- src/components/Mealtomeal.tsx
+++ src/components/Mealtomeal.tsx
@@ -4,7 +4,6 @@ import homeIcon from "../img/skills/home-icon.png";
 import githubIcon from "../img/skills/github-icon.png";
 import diaryIcon from "../img/skills/diary-icon.png";
 import login from "../img/projects/Login.gif";
-import phone from "../img/projects/PhoneVerification.gif";
 import reviewUpload from "../img/projects/ReviewUpload.gif";
 import searchBar from "../img/projects/SearchBar.gif";
 import signup from "../img/projects/SignUp.gif";
@@ -32,30 +31,27 @@ function Mealtomeal() {
                 <nav className="project-info-container">
                     <div className="page-carousel">
                         <Slider {...settings}>
-                            <img src={login} alt="" />
+                            <img className="project-landing-image" src={login} alt="" />
 
-                            <img src={phone} alt="" />
+                            <img className="project-landing-image" src={reviewUpload} alt="" />
 
-                            <img src={reviewUpload} alt="" />
+                            <img className="project-landing-image" src={searchBar} alt="" />
 
-                            <img src={searchBar} alt="" />
+                            <img className="project-landing-image" src={signup} alt="" />
 
-                            <img src={signup} alt="" />
-
-                            <img src={storeInfo} alt="" />
+                            <img className="project-landing-image" src={storeInfo} alt="" />
                         </Slider>
                     </div>
-                    {/* <img className="project-landing-image" src={mealtomeal} alt="" /> */}
                     <div className="project-details-container">
                         <nav className="details-title">🍔 내가 먹은 맛있는 한 끼를 다른 사람에게 기부할 수 있는 플랫폼 입니다</nav>
-                        <ul>누군가가 한 끼를 먹을 수 있도록 기부할 수 있어요</ul>
-                        <ul>누구든지 다른 사람이 기부한 음식으로 끼니를 챙길 수 있어요</ul>
-                        <ul>음식점 사장님들은 손님들이 남긴 리뷰로 맛집임을 증명 할 수 있어요</ul>
+                        <li className="details-sentence">누군가가 한 끼를 먹을 수 있도록 기부할 수 있어요</li>
+                        <li className="details-sentence">누구든지 다른 사람이 기부한 음식으로 끼니를 챙길 수 있어요</li>
+                        <li className="details-sentence">음식점 사장님들은 손님들이 남긴 리뷰로 맛집임을 증명 할 수 있어요</li>
                         <nav className="details-title">✏️ 주요 기능 구현</nav>
-                        <ul>프로젝트 SR 기획 및 진행 상황 관리, 팀원들 간 소통 관리</ul>
-                        <ul>Google Maps API를 이용한 지도 및 인포 윈도우, 검색창 구현</ul>
-                        <ul>S3를 활용한 이미지 업로드 구현</ul>
-                        <ul>카카오 로그인 API를 이용한 토큰 기반 소셜 로그인 구현</ul>
+                        <li className="details-sentence">프로젝트 SR 기획 및 진행 상황 관리, 팀원들 간 소통 관리</li>
+                        <li className="details-sentence">Google Maps API를 이용한 지도 및 인포 윈도우, 검색창 구현</li>
+                        <li className="details-sentence">S3를 활용한 이미지 업로드 구현</li>
+                        <li className="details-sentence">카카오 로그인 API를 이용한 토큰 기반 소셜 로그인 구현</li>
                         <nav className="project-stack-container">
                             <div className="stack-box">#React S3</div>
                             <div className="stack-box">#Sass</div>
