9ce19cc4ef71bc519963cddd193eb334f66994ba
jamiep9rk <jamiecareer96@gmail.com>
2022-01-19 20:15:27 +0900

    ADD: Landing 마우스 스크롤 애니메이션 추가, About 메일과 깃헙 주소 호버 효과 추가

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -12,15 +12,10 @@ function Landing() {
                 </div>
             </nav>
             <nav className="scroll-container">
-                <a className="scroll-link" href="#content">
-                    <svg className="mouse" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 76 130" preserveAspectRatio="xMidYMid meet">
-                        <g fill="none" fill-rule="evenodd">
-                            <rect width="70" height="118" x="1.5" y="1.5" stroke="black" stroke-width="3" rx="36" />
-                            <circle className="scroll" cx="36.5" cy="31.5" r="4.5" fill="black" />
-                        </g>
-                    </svg>
-                </a>
-                <div className="scroll-text">scroll</div>
+                <div className="main-scroll-icon">
+                    <div className="main-scroll-icon-down" />
+                </div>
+                <span className="main-scroll-icon-text">Scroll</span>
             </nav>
         </div>
     );
