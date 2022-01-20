f2dea8efe1f64ba75b9e428b3ef92d78358c6dd7
jamiep9rk <jamiecareer96@gmail.com>
2022-01-20 22:51:10 +0900

    ADD: Projects 페이지 반응형 Mobile과 Tablet 버전 추가, Footer 반응형 Mobile 버전 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,13 @@ import { Link } from "react-scroll";
 function Footer() {
     return (
         <footer className="footer-container">
+            <nav className="footer-contact-container">
+                <i className="far fa-hand-point-right" />
+                <Link className="footer-contact-link" to="about-container" spy={true} smooth={true}>
+                    <div className="footer-contact">Contact</div>
+                </Link>
+            </nav>
             <nav className="footer-title">@ 2022 Front-end developer, Jamie Park's portfolio</nav>
-            <i className="far fa-hand-point-right" />
-            <Link className="footer-contact-link" to="about-container" spy={true} smooth={true}>
-                <div className="footer-contact">Contact</div>
-            </Link>
         </footer>
     );
 }
